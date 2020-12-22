415d18b69a59bd9d9bfda4ae4651425ce55db28b
Nurul Huda Sulton <nhsltndev@gmail.com>
2020-12-22 14:52:12 +0700

    Enchanced Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,20 +4,26 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
-    const transformingredients = Object.keys(props.ingredients) // Convert object of state to array
+    let transformedIngredients = Object.keys(props.ingredients) // Convert object of state to array
         .map(igKey => { // Get Value of ingredients state ex = [cheese, bacon, salad, meat] 
             return [...Array(props.ingredients[igKey])] // get value of nothing array from igkey = [4) [Array(1), Array(1), Array(2), Array(2)] which all array is undefinied because we just want get the how much ingredient
                 .map((_, i) => { 
                     return <BurgerIngredients key={igKey + i} type={igKey} /> // from array of nothing we populated it value, props from igKey so Array(2) [key = cheese0/cheese1. props = cheese, and the typeof]
                 })
-        });
+        })
+        .reduce((arr, el) => { // reduce to make it into into single aray combining(concat) from all el so results will be [6] [salad1, cheese1, cheese2, meat1, meat2, bacon] from [Array(1), Array(1), Array(2), Array(2)] 
+            return arr.concat(el)
+        }, []);
+        if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please Start Adding Ingredients!</p>;
+        }
 
-    // console.log(transformingredients); check hasil di console
+    console.log(transformedIngredients);
 
     return(
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top"/>
-            {transformingredients}
+            {transformedIngredients}
             <BurgerIngredients type="bread-bottom"/>
         </div>
     );
