d6d903ddd9b4c5b5ce864f6b46b82388855bef5b
Nurul Huda Sulton <nhsltndev@gmail.com>
2020-12-26 02:52:47 +0700

    Enchanced SideDraw with Change Layout into Stateful

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux'
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
-    //..
+    let attachClass = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachClass = [classes.SideDrawer, classes.Open];
+    }     
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachClass.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
