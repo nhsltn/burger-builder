0f69741ac887cbbf244b68d9ad39909f97b91461
Nurul Huda Sulton <nhsltndev@gmail.com>
2020-12-26 02:31:57 +0700

    Responsive SideBar & Toolbar on steroids

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,7 +8,9 @@ const sideDrawer = (props) => {
     //..
     return(
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
@@ -16,4 +18,4 @@ const sideDrawer = (props) => {
     );
 };
 
-export default sideDrawer;
+export default sideDrawer; 
