dbb5dc217d517640a52a6249048bda3d9f09e905
Nurul Huda Sulton <nhsltndev@gmail.com>
2020-12-30 17:25:01 +0700

    Send Post Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: .5,
@@ -77,7 +78,27 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!');
+        // alert('You Continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customerData: {
+                name: "Sulton",
+                address: {
+                    street: 'Kamper',
+                    zipCode: '15343',
+                    country: 'Indonesia'
+                }, email : 'asdasd@gmail.com'
+            },
+            deliveryMethod: 'economy'
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
