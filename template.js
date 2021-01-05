33eeb935d52e7e6403b72e209ad0345897c6d1fe
Nurul Huda Sulton <nhsltndev@gmail.com>
2021-01-05 17:00:51 +0700

    update tempate for both class and funtion component

--- template.js
+++ template.js
@@ -5,3 +5,11 @@ const template = (props) => (
 );
 
 export default template;
+
+import React, {Component} from 'react';
+
+class Orders extends Component {
+    return ();
+}
+
+export default Orders;
