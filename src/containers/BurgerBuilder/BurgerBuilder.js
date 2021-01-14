ef08760a1562a6d16990a8654b98cdec46c9f39d
Nurul Huda Sulton <nhsltndev@gmail.com>
2021-01-15 03:40:56 +0700

    Redux & UI State

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,8 +12,7 @@ import axios from '../../axios-orders';
 import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
-    state = {        
-        purchaseable: false,
+    state = {                
         purchasing: false,
         loading: false,
         error: false
@@ -38,7 +37,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({purchaseable: sum > 0});
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -90,7 +89,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded} 
                         ingredientsRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchasedState(this.props.igd)}
                         price={this.props.price}
                         ordered={this.purchaseHandler} />
                 </Aux>
